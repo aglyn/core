f437533d516d142e1ba2658b591169ca42c7dc41
Zach Gover <9044773+zgover@users.noreply.github.com>
2022-02-05 04:46:05 -0600

    refactor: renamed app 'cloud' to 'api' for clarity of its purpose

--- apps/api/jest.config.js
+++ apps/api/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  displayName: 'cloud',
+  displayName: 'api',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/cloud',
+  coverageDirectory: '../../coverage/apps/api',
 }
