78ee3fd0a526850f126c05e67882382993695a1e
Zach Gover <9044773+zgover@users.noreply.github.com>
2026-05-27 22:24:03 -0500

    fix: build issues
    - fix: adhoc fix for build issues from 38b188c Reapply "refactor: canvas
    actions"

--- libs/aglyn/src/lib/canvas-manager/stores/node.store.ts
+++ libs/aglyn/src/lib/canvas-manager/stores/node.store.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright 2024 Aglyn LLC
+ * Copyright 2026 Aglyn LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@ import { PluginId } from '../../plugin-manager'
 import { ComponentId, NodeId, NodeType } from '../../types'
 
 // Interface for NodeModel
-interface INodeModel {
+interface INodeModel<P = any> {
   $id: string
   nodes?: NodeId[]
   name?: string
@@ -41,19 +41,29 @@ const NodeModel = types.model<INodeModel>('NodeModel', {
   //   // then typecast each array element to Instance<typeof INodeModel>
   //   types.array(types.late((): IAnyModelType => NodeModel)),
   // ),
-  nodes: types.array(types.string),
-  name: types.string,
-  type: types.string,
-  pluginId: types.string,
-  componentId: types.string,
-  parentId: types.string,
-  props: types.model,
-  sx: types.model,
-  className: types.string,
+  nodes: types.maybe(types.array(types.string)),
+  name: types.maybe(types.string),
+  type: types.maybe(types.string),
+  pluginId: types.maybe(types.string),
+  componentId: types.maybe(types.string),
+  parentId: types.maybe(types.string),
+  props: types.maybe(types.frozen()),
+  sx: types.maybe(types.frozen()),
+  className: types.maybe(types.string),
+})
+
+// Interface for LeafModel
+interface ILeafModel {
+  aa: string
+}
+
+// Define a leaf-level model
+const LeafModel = types.model('LeafModel', {
+  aa: types.maybe(types.string),
 })
 
 const CanvasModel = types.model('NodeStoreModel', {
-  nodes: types.map(NodeModel),
+  nodes: types.maybe(types.map(NodeModel)),
 })
 
 // Define the root-level model with a reference to child nodes
